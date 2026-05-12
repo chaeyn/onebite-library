0751b7145c6eb145e244d1edb075a6004d5c1302
chaeyn <chaulfe@gmail.com>
2026-05-12 19:51:13 +0900

    zustand middleware - combine, subscribeWithSelector, persist, createJSONStorage, devtools

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,24 +1,56 @@
 import { create } from "zustand";
-import { combine } from "zustand/middleware";
+import {
+  combine,
+  subscribeWithSelector,
+  persist,
+  createJSONStorage,
+  devtools,
+} from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 export const useCountStore = create(
-  immer(
-    combine({ count: 0 }, set => ({
-      actions: {
-        increaseOne: () => {
-          set(state => {
-            state.count += 1;
-          });
-        },
-        decreaseOne: () => {
-          set(state => {
-            state.count -= 1;
-          });
-        },
-      },
-    }))
+  devtools(
+    persist(
+      subscribeWithSelector(
+        immer(
+          combine({ count: 0 }, set => ({
+            actions: {
+              increaseOne: () => {
+                set(state => {
+                  state.count += 1;
+                });
+              },
+              decreaseOne: () => {
+                set(state => {
+                  state.count -= 1;
+                });
+              },
+            },
+          }))
+        )
+      ),
+      {
+        name: "countStore",
+        partialize: store => ({ count: store.count }),
+        storage: createJSONStorage(() => sessionStorage),
+      }
+    ),
+    {
+      name: "countStore",
+    }
   )
+);
+
+useCountStore.subscribe(
+  store => store.count,
+  (count, prevCount) => {
+    // Listener
+    console.log(`${prevCount} -> ${count}`);
+    const store = useCountStore.getState();
+    useCountStore.setState(store => {
+      // count: 10;
+    });
+  }
 );
 
 export const useCount = () => {
