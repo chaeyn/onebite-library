5322ef8ada87a7139e12ae6f1389fd71bd5deb5c
chaeyn <chaulfe@gmail.com>
2026-05-13 12:32:35 +0900

    캐싱 메커니즘 2

--- src/hooks/queries/use-todo-data-by-id.ts
+++ src/hooks/queries/use-todo-data-by-id.ts
@@ -5,8 +5,11 @@ export function useTodoDataById(id: number) {
   return useQuery({
     queryFn: () => fetchTodoById(id),
     queryKey: ["todos", id],
+
     retry: 1,
-    staleTime: 5000,
+    staleTime: 300000,
+    gcTime: 5000, // 가비지 컬렉터 시간 (inactive -> GC excute time)
+
     // refetchInterval: 1000, -> 1초마다 refetch
     // refetchOnMount: false,
     // refetchOnWindowFocus: false,
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,20 @@ import { BrowserRouter } from "react-router";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 5,
+      staleTime: 0,
+      gcTime: 5 * 60 * 1000,
+
+      refetchOnMount: true,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchInterval: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
