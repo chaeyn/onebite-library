1b781e6a9d902c9f289ef39722ecc5ffb834ceb6
chaeyn <chaulfe@gmail.com>
2026-05-19 17:35:31 +0900

    낙관적 업데이트 1

--- src/components/todo-list/todo-item.tsx
+++ src/components/todo-list/todo-item.tsx
@@ -1,22 +1,24 @@
 import { Button } from "@/components/ui/button.tsx";
-import { useDeleteTodo } from "@/store/todos.ts";
 import { Link } from "react-router";
+import type { Todo } from "@/types.ts";
+import { useUpdateTodoMutation } from "@/hooks/mutations/use-update-todo-mutation.ts";
 
-interface TodoItemProps {
-  id: number;
-  content: string;
-}
-
-export default function TodoItem({ id, content }: TodoItemProps) {
-  const deleteTodo = useDeleteTodo();
-
-  const handleDeleteClick = () => {
-    deleteTodo(id);
+export default function TodoItem({ id, content, isDone }: Todo) {
+  const { mutate } = useUpdateTodoMutation();
+  const handleDeleteClick = () => {};
+  const handleCheckboxClick = () => {
+    mutate({
+      id: id,
+      isDone: !isDone,
+    });
   };
 
   return (
     <div className="flex items-center justify-between rounded-md border p-2">
-      <Link to={`/todolist/${id}`}>{content}</Link>
+      <div className="flex gap-5">
+        <input type="checkbox" checked={isDone} onClick={handleCheckboxClick} />
+        <Link to={`/todolist/${id}`}>{content}</Link>
+      </div>
       <Button onClick={handleDeleteClick} variant="destructive">
         삭제
       </Button>
