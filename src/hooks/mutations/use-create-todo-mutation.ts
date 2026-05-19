60f738b5e6d1244f480e8ff7523400efe8720268
chaeyn <chaulfe@gmail.com>
2026-05-19 17:16:14 +0900

    setQueryData로 응답 결과값을 이용하여 refetch 없이 업데이트

--- src/hooks/mutations/use-create-todo-mutation.ts
+++ src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createTodo } from "@/api/create-todo.ts";
 import { QUERY_KEYS } from "@/lib/constants.ts";
+import type { Todo } from "@/types.ts";
 
 export function useCreateTodoMutation() {
   const queryClient = useQueryClient();
@@ -9,9 +10,10 @@ export function useCreateTodoMutation() {
     mutationFn: createTodo,
     onMutate: () => {},
     onSettled: () => {},
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: QUERY_KEYS.todo.list,
+    onSuccess: newTodo => {
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, prevTodos => {
+        if (!prevTodos) return [newTodo];
+        return [...prevTodos, newTodo];
       });
     },
     onError: error => {
