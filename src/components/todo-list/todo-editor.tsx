8c918d11b713ae02dfd3acc8f77e5e49bece0eaa
chaeyn <chaulfe@gmail.com>
2026-05-19 16:58:57 +0900

    useMutation

--- src/components/todo-list/todo-editor.tsx
+++ src/components/todo-list/todo-editor.tsx
@@ -1,15 +1,15 @@
 import { Input } from "@/components/ui/input.tsx";
 import { Button } from "@/components/ui/button.tsx";
-import { useCreateTodo } from "@/store/todos.ts";
 import { useState } from "react";
+import { useCreateTodoMutation } from "@/hooks/mutations/use-create-todo-mutation.ts";
 
 export default function TodoEditor() {
+  const { mutate, isPending } = useCreateTodoMutation();
   const [content, setContent] = useState("");
-  const createTodo = useCreateTodo();
 
   const handleAddClick = () => {
     if (content.trim() === "") return;
-    createTodo(content);
+    mutate(content);
     setContent("");
   };
 
@@ -20,7 +20,9 @@ export default function TodoEditor() {
         onChange={e => setContent(e.target.value)}
         placeholder="새로운 할 일을 입력하세요..."
       />
-      <Button onClick={handleAddClick}>추가</Button>
+      <Button onClick={handleAddClick} disabled={isPending}>
+        추가
+      </Button>
     </div>
   );
 }
